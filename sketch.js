883d33df1a6833470de021eece5c85654963a05b
JacobinaByjus <89422919+JacobinaByjus@users.noreply.github.com>
2021-09-02 18:51:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function findNum(arr, x) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === x) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function setup() {
+  createCanvas(400, 400);
+  var result = findNum(arr, 10);
+  if (result == -1) {
+    console.log("número não encontrado");
+  } else {
+    console.log("número encontrado no index:", result);
+  }
+}
+
+function draw() {
+  background(220);
+}
